7b446c301d054afb7c4c1de64fc609c21c057e3f
Ville Halldin <villehalldin05@gmail.com>
2026-04-14 15:59:32 +0200

    Stop using aid in inventory lol

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,23 +1,38 @@
-use std::sync::mpsc::{Receiver};
+use std::{
+    sync::Mutex,
+    sync::Arc,
+};
 
 struct Inventory {
-    items: usize, // I'm thinking a list or a hashmap or something, array would also work since the inventory size should probably be fixed
+    items: Arc<Mutex<(String, usize)>>, // Resources are stored as (String, usize) where String is name of resource and usize is count
 }
 
 pub mod inventory {
-    use std::sync::mpsc;
-    use crate::aid::{self, AID};
+    use std::{
+        sync::{Arc, Mutex}, thread::{self, JoinHandle}
+    };
 
-    use crate::inventory::Inventory;
+    use crate::{inventory::Inventory};
 
-    pub fn init<T>(AID: AID<T>, receiver: mpsc::Receiver<T>) { // Not completely sure how the invariant is supposed to be used.
-        let inventory: Inventory = Inventory { items: 0 };
-        main_loop(inventory);
+    pub fn init(name: String) -> (JoinHandle<()>, Arc<Mutex<(String, usize)>>) {
+        let inv_mut: Arc<Mutex<(String, usize)>> = Arc::new(Mutex::new((String::from("Mutexium"), 0)));
+        let inventory: Inventory = Inventory { items: inv_mut.clone() };
+
+        let handle: thread::JoinHandle<()> = thread::spawn(|| main_loop(inventory, name));
+
+        return (handle, inv_mut);
     }
 
-    fn main_loop(inventory: Inventory) {
+    fn main_loop(inventory: Inventory, name: String) {
         loop {
+            // TODO: Do something here, this is just a placeholder
+            println!("In {}...", name);
 
+            let mut contents = inventory.items.lock().unwrap();
+
+            (*contents).1 += 1;
+
+            println!("{0} - {1}", (*contents).0, (*contents).1);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,24 @@ use crate::aid::AID;
 mod inventory;
 mod aid;
 
+use std::{
+    sync::{Arc, Mutex, MutexGuard},
+};
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn test_inventory() {
+    let mut inventories: Vec<Arc<Mutex<(String, usize)>>> = Vec::new();
+
+    for i in 1..4 {
+        inventories.push(inventory::inventory::init(format!("Thread {}", i)).1); 
+    }
+
+    let mut inv_1: MutexGuard<'_, (String, usize)> = inventories[0].lock().unwrap();
+
+    (*inv_1).1 = 0;
+
+    loop {} // loop forever so the program doesn't terminate and kill the threads
 }
