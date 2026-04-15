632117127ddb7d1edbef00ba230b35a98c373e62
Ville Halldin <villehalldin05@gmail.com>
2026-04-15 15:17:23 +0200

    Change inventory to be Arc and all that that entails (non-functioning)

--- src/inventory.rs
+++ src/inventory.rs
@@ -3,63 +3,114 @@ use std::{
     sync::Arc,
 };
 
-struct Inventory {
+use crate::{
+    aid::AID, 
+};
+
+struct Inventory { // Think about moving the Arc to be the entire inventory
+    max: usize,
     items: Arc<Mutex<(String, usize)>>, // Resources are stored as (String, usize) where String is name of resource and usize is count
+}
+
+pub enum InventoryMessage {
+    // The following are sent by owner (entity)
+    Increase,
+    Decrease,
+    TakeFrom(AID<InventoryMessage>), 
+    Kill,
+
+    // The following are sent by another inventory
+    MoveTo(Inventory),  
 }
 
 pub mod inventory {
     use std::{
-        sync::{Arc, Mutex, MutexGuard}, thread::{self, JoinHandle}
+        sync::{Arc, Mutex, MutexGuard}
     };
 
-    use crate::{inventory::Inventory};
+    use crate::{
+        aid::AID, 
+        inventory::{Inventory, InventoryMessage}
+    };
 
-    pub fn init() -> (JoinHandle<()>, Arc<Mutex<(String, usize)>>) {
-        let inv_mut: Arc<Mutex<(String, usize)>> = Arc::new(Mutex::new((String::from("Mutexium"), 0)));
-        let inventory: Inventory = Inventory { items: inv_mut.clone() };
-
-        let handle: thread::JoinHandle<()> = thread::spawn(|| main_loop(inventory));
-
-        return (handle, inv_mut);
+    pub fn init() -> AID<InventoryMessage> {
+        return AID::new(main_loop);
     }
 
-    fn main_loop(inventory: Inventory) {
+    fn construct_inventory() -> Inventory {
+        let inv_arc: Arc<Mutex<(String, usize)>> = Arc::new(
+                                                Mutex::new((String::from("Mutexium"), 0))
+                                                    );
+        return Inventory { max: 10, items: inv_arc };
+    }
+
+    fn main_loop(_aid: AID<InventoryMessage>, mailbox: std::sync::mpsc::Receiver<InventoryMessage>) {
+        let inventory: Inventory = construct_inventory();
+
         loop {
-            // // TODO: Do something here, this is just a placeholder
-            // println!("In {}...", name);
+            for msg in &mailbox {
+                match msg {
+                    InventoryMessage::Increase => increase(&inventory),
+                    InventoryMessage::Decrease => decrease(&inventory),
+                    InventoryMessage::TakeFrom(other) => take_from(&inventory, other),
+                    InventoryMessage::Kill => return,
 
-            // let mut contents = inventory.items.lock().unwrap();
-
-            // (*contents).1 += 1;
-
-            // println!("{0} - {1}", (*contents).0, (*contents).1);
+                    InventoryMessage::MoveTo(other_inventory) => move_to(&inventory, &other_inventory), // This is a placeholder
+                };
+            }
         }
     }
 
-    /*
-        These functions should be in the entities (probably) and not public
-        nvm i think this works since this is a module that can just be imported
-     */
-    pub fn increase(/*inventory: Inventory or*/ items: &Arc<Mutex<(String, usize)>>) {
-        let mut contents: MutexGuard<'_, (String, usize)> = items.lock().unwrap(); 
+    // Increase this inventory by one, will not do anything if inventory is full.
+    fn increase(inventory: &Inventory) {
+        if is_full(inventory) {
+            return; // Error or something
+        }
+
+        let mut contents: MutexGuard<'_, (String, usize)> = inventory.items.lock().unwrap(); 
         (*contents).1 += 1;
+
+        return; // Success or something
     }
 
-    pub fn decrease(/*inventory: Inventory or*/ items: &Arc<Mutex<(String, usize)>>) {
-        let mut contents: MutexGuard<'_, (String, usize)> = items.lock().unwrap(); 
+    // Decrease this inventory by one, will not do anything if inventory is empty.
+    fn decrease(inventory: &Inventory) {
+        if is_empty(inventory) {
+            return; // Error or something
+        }
+
+        let mut contents: MutexGuard<'_, (String, usize)> = inventory.items.lock().unwrap(); 
         (*contents).1 -= 1;
+
+        return; // Success or something
     }
 
-    /**
-     * Moves from from to to
-     */
-    pub fn r#move(from: &Arc<Mutex<(String, usize)>>, to: &Arc<Mutex<(String, usize)>>) {
+    // Moves an item from 'from to 'to'. 
+    fn move_to(from: &Inventory, to: &Inventory) {
+        if is_empty(from) || is_full(to) {
+            return; // ERROR or something
+        }
+
         decrease(from);
         increase(to);
+
+        return; // Success or something
+    }
+
+    fn take_from(inventory: &Inventory, aid: AID<InventoryMessage>) {
+        aid.send(InventoryMessage::MoveTo(inventory));
+    }
+
+    fn is_empty(inventory: &Inventory) -> bool { // Could be a method of Inventory
+        return (*inventory.items.lock().unwrap()).1 == 0;
+    }
+
+    fn is_full(inventory: &Inventory) -> bool { // Could be a method of Inventory
+        return (*inventory.items.lock().unwrap()).1 == inventory.max;
     }
 
     // For debugging as of now
-    pub fn print_inv(this_items: &Arc<Mutex<(String, usize)>>, name: String) {
+    fn print_inv(this_items: &Arc<Mutex<(String, usize)>>, name: String) {
         let contents: MutexGuard<'_, (String, usize)> = this_items.lock().unwrap();
 
         println!("{0} - {1}", name, (*contents).1);
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,6 @@
-use crate::aid::AID;
-
 mod inventory;
 mod aid;
 
-use core::time;
-use std::{
-    sync::{Arc, Mutex, MutexGuard}, thread::sleep,
-};
-
 fn main() {
     println!("Hello, world!");
-    test_inventory2();
-}
-
-fn test_inventory() {
-    let mut inventories: Vec<Arc<Mutex<(String, usize)>>> = Vec::new();
-
-    for i in 1..4 {
-        inventories.push(inventory::inventory::init().1); 
-    }
-
-    let mut inv_1: MutexGuard<'_, (String, usize)> = inventories[0].lock().unwrap();
-
-    (*inv_1).1 = 0;
-
-    loop {} // loop forever so the program doesn't terminate and kill the threads
-}
-
-fn test_inventory2() {
-    let inv_a: Arc<Mutex<(String, usize)>> = inventory::inventory::init().1;
-    let inv_b: Arc<Mutex<(String, usize)>> = inventory::inventory::init().1;
-
-    for _i in 0..10 {
-        inventory::inventory::increase(&inv_a);
-    }
-
-    for _i in 0..10 {
-        inventory::inventory::r#move(&inv_a, &inv_b);
-
-        inventory::inventory::print_inv(&inv_a, String::from("Inventory A"));
-        inventory::inventory::print_inv(&inv_b, String::from("Inventory B"));
-
-        sleep(time::Duration::from_millis(500));
-    }
 }
