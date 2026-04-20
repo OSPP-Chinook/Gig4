5ebc366975dc0b45540706dd68a380bb26270a84
Ville Halldin <villehalldin05@gmail.com>
2026-04-21 00:13:50 +0200

    Non functioning updated invenotry

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,49 +1,79 @@
+use std::{collections::HashMap};
+
 use crate::{
     aid::AID, 
 };
 
-struct Inventory {
-    aid: AID<InventoryMessage>,
-    max: usize,
-    items: (String, usize), // Resources are stored as (String, usize) where String is name of resource and usize is count
+#[derive(Clone)]
+pub enum Item {
+    Mutexium,
 }
 
-impl Inventory {
-    fn construct(aid: AID<InventoryMessage>) -> Self {
-        return Inventory { aid, max: 10, items: (String::from("Mutexium"), 0) };
-    }
-
-    fn is_empty(&self) -> bool {
-        return self.items.1 == 0;
-    }
-
-    fn is_full(&self) -> bool {
-        return self.items.1 == self.max;
-    }
-
-    // For debugging as of now
-    fn print_inv(&self, name: String) {
-        println!("{0}: {1} - {2}/{3}", name, self.items.0, self.items.1, self.max);
+impl Item {
+    fn to_string(&self) -> &str {
+        match self {
+            Item::Mutexium => return "Mutexium",
+        };
     }
 }
 
 #[derive(Clone)]
 pub enum InventoryMessage {
     // The following are sent by owner (entity)
-    Increase,
-    Decrease,
-    TakeFrom(AID<InventoryMessage>), 
+    Add((Item, usize)),
+    Remove((Item, usize)),
+    TakeFrom(AID<InventoryMessage>, (Item, usize)), 
+    GiveTo(AID<InventoryMessage>, (Item, usize)),
     Kill,
 
     // The following are sent by another inventory
-    GiveMeItem(AID<InventoryMessage>),                  // SHOULD ONLY BE CALLED USING TakeFrom MESSAGE
-    ReceiveItem(Result<(String, usize), &'static str>), // SHOULD ONLY BE CALLED USING GiveMeItem MESSAGE
+    GiveMeItem(AID<InventoryMessage>),                       // From TakeFrom 
+    GiveMeItemResult(Result<(Item, usize), &'static str>), // From GiveMeItem
+    TakeMyItem(AID<InventoryMessage>, (Item, usize)),        // From GiveTo
+    TakeMyItemResult(Result<(Item, usize), &'static str>),   // From TakeMyItem
+}
+
+
+struct Inventory {
+    aid: AID<InventoryMessage>,
+    max: usize,
+    waiting: Vec<AID<InventoryMessage>>,
+    // items: (String, usize), // Resources are stored as (String, usize) where String is name of resource and usize is count
+    items: HashMap<Item, (usize, usize)>, // Key: Name of item -> Value: (count, max).
+}
+
+impl Inventory {
+    fn construct(aid: AID<InventoryMessage>) -> Self {
+        // return Inventory { aid, max: 10, waiting: Vec::new(), items: (String::from("Mutexium"), 0) };
+        return Inventory { aid, max: 10, waiting: Vec::new(), items: HashMap::new() };
+    }
+
+    fn is_empty(&self) -> bool {
+        return self.items.1 == 0; // FIX
+    }
+
+    fn is_full(&self) -> bool {
+        return self.items.1 == self.max; // FIX
+    }
+
+    // For debugging as of now
+    fn print_inv(&self, name: String) {
+        println!("{0}:\n", name);
+        
+        for (key, value) in &self.items {
+            println!("      {0} - {1}/{2}\n", key.to_string(), value.0, value.1);
+        }
+    }
 }
 
 pub mod inventory {
     use crate::{
         aid::AID, 
-        inventory::{Inventory, InventoryMessage}
+        inventory::{
+            Inventory, 
+            InventoryMessage, 
+            Item
+        }
     };
 
 
@@ -60,19 +90,25 @@ pub mod inventory {
         let mut transfer_in_process: bool = false; 
 
         loop {
-            match mailbox.recv().unwrap() { // Recv blocks the thread until a message is recieved which seems right for this
-                InventoryMessage::Increase => increase(&mut inventory),
-                InventoryMessage::Decrease => decrease(&mut inventory),
-                InventoryMessage::TakeFrom(other) => 
-                    take_from(&inventory, other, &mut transfer_in_process),
+            match mailbox.recv().unwrap() {
+                InventoryMessage::Add(item) => add(&mut inventory, item),
+                InventoryMessage::Remove(item) => remove(&mut inventory, item),
+                InventoryMessage::TakeFrom(other, items) => 
+                    take_from(&inventory, other, items, &mut transfer_in_process),
+                InventoryMessage::GiveTo(other, items) => 
+                    give_to(&inventory, other, items, &mut transfer_in_process),
                 InventoryMessage::Kill => return, 
 
                 InventoryMessage::GiveMeItem(sender) => 
                     give_me_item(&mut inventory, sender, &mut transfer_in_process),
-                InventoryMessage::ReceiveItem(result) => {
-                    receive_item(&mut inventory, result);
+                InventoryMessage::GiveMeItemResult(result) => {
+                    give_me_item_result(&mut inventory, result);
                     transfer_in_process = false;
                 }
+                InventoryMessage::TakeMyItem(sender, item) => 
+                    take_my_item(&inventory, sender, item, &mut transfer_in_process),
+                InventoryMessage::TakeMyItemResult(result) => 
+                    take_my_item_result(&mut inventory, result),
             };
 
             inventory.print_inv(String::from("Inventory"));
@@ -84,7 +120,7 @@ pub mod inventory {
     /// # Arguments
     /// 
     /// * 'inventory' - Mutable reference to the inventory to increase
-    fn increase(inventory: &mut Inventory) {
+    fn add(inventory: &mut Inventory, item: (Item, usize)) {
         if inventory.is_full() {
             return; // Error or something
         }
@@ -99,7 +135,7 @@ pub mod inventory {
     /// # Arguments
     /// 
     /// * 'inventory' - Mutable reference to the inventory to decrease
-    fn decrease(inventory: &mut Inventory) {
+    fn remove(inventory: &mut Inventory, item: (Item, usize)) {
         if inventory.is_empty() {
             return; // Error or something
         }
@@ -120,6 +156,7 @@ pub mod inventory {
     fn take_from(
         inventory: &Inventory, 
         aid: AID<InventoryMessage>, 
+        items: (Item, usize),
         transfer_in_progress: &mut bool
     ) {
         if *transfer_in_progress || inventory.is_full() {
@@ -128,6 +165,15 @@ pub mod inventory {
 
         *transfer_in_progress = true;
         _ = aid.send(InventoryMessage::GiveMeItem(inventory.aid.clone())); // Should handle Result in some way
+    }
+
+    fn give_to(
+        inventory: &Inventory, 
+        aid: AID<InventoryMessage>, 
+        items: (Item, usize),
+        transfer_in_progress: &mut bool
+    ) {
+
     }
 
     /// Checks if this inventory can give item and sends a result containing either a tuple containing
@@ -144,18 +190,18 @@ pub mod inventory {
         transfer_in_progress: &mut bool
     ) {
         if *transfer_in_progress {
-            _ = sender.send(InventoryMessage::ReceiveItem(Result::Err("I'm busy!"))); // Sends an error; TODO: Should handle Result in some way
+            _ = sender.send(InventoryMessage::GiveMeItemResult(Result::Err("I'm busy!"))); // Sends an error; TODO: Should handle Result in some way
             return;
         }
 
         if inventory.is_empty() {
-            _ = sender.send(InventoryMessage::ReceiveItem(Result::Err("I'm empty!"))); // Sends an error; TODO: Should handle Result in some way
+            _ = sender.send(InventoryMessage::GiveMeItemResult(Result::Err("I'm empty!"))); // Sends an error; TODO: Should handle Result in some way
             return;
         }
 
 
         inventory.items.1 -= 1;
-        _ = sender.send(InventoryMessage::ReceiveItem(Result::Ok((inventory.items.0.clone(), 1)))); // Sends a tuple containing what item it is and how many it moved; TODO: Should handle Result in some way
+        _ = sender.send(InventoryMessage::GiveMeItemResult(Result::Ok((inventory.items.0.clone(), 1)))); // Sends a tuple containing what item it is and how many it moved; TODO: Should handle Result in some way
     }
 
     /// Gets the result from a GiveMeItem message and add the item to this inventory, or prints the
@@ -166,10 +212,26 @@ pub mod inventory {
     /// * 'inventory' - Mutable reference to this inventory
     /// * 'result'    - A result containing either a tuple of what item was moved and the quantity, 
     ///                 or the error message as a str
-    fn receive_item(inventory: &mut Inventory, result: Result<(String, usize), &'static str>) {
+    fn give_me_item_result(inventory: &mut Inventory, result: Result<(Item, usize), &'static str>) {
         match result {
             Ok(item) => { inventory.items.1 += item.1; }, // This is a placeholder
             Err(msg) => { println!("{}", msg) } // should probably do something else
         };
+    }
+
+    fn take_my_item(
+        inventory: &Inventory, 
+        sender: AID<InventoryMessage>, 
+        items: (Item, usize), 
+        transfer_in_progress: &mut bool
+    ) {
+
+    }
+
+    fn take_my_item_result(
+        inventory: &mut Inventory, 
+        result: Result<(Item, usize), &'static str>
+    ) {
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
-use core::time;
-use std::{thread::sleep};
-
 mod inventory;
 mod aid;
+
+use inventory::{
+    Item,
+    InventoryMessage,
+};
 
 fn main() {
     println!("Hello, world!");
 }
 
 fn test_inventory() {
-    let inventory_aid: aid::AID<inventory::InventoryMessage> = inventory::inventory::init();
-    let inventory_aid2: aid::AID<inventory::InventoryMessage> = inventory::inventory::init();
+    let inventory_aid: aid::AID<InventoryMessage> = inventory::inventory::init();
+    let inventory_aid2: aid::AID<InventoryMessage> = inventory::inventory::init();
 
-    for _ in 0..9 {
-        _ = inventory_aid.send(inventory::InventoryMessage::Increase); // Minskar inventoryt med 1, om det inte redan är tomt
-        sleep(time::Duration::from_millis(500));
-    }
+    _ = inventory_aid.send(InventoryMessage::Add((Item::Mutexium, 9))); // Adds 9 Mutexium to inventory 1
 
     println!("Moving from inventory 1 to 2");
 
-    for _ in 0..10 { // One more than there are items, inventory 1 will return an error that is printed
-        _ = inventory_aid2.send(inventory::InventoryMessage::TakeFrom(inventory_aid.clone()));
-        sleep(time::Duration::from_millis(500));
-    }
+    _ = inventory_aid2.send(InventoryMessage::TakeFrom(inventory_aid.clone(), (Item::Mutexium, 10))); // One more than there are items, inventory 1 will return an error that is printed
 
     loop{}
 }
