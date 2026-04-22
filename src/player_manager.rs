fb9582632f0f7bcd4d25f790857960c5b6620bdc
Didrik <coraxites@gmail.com>
2026-04-22 11:33:59 +0200

    added basic grid renderer

---
+++ src/player_manager.rs
@@ -0,0 +1,44 @@
+use ratatui::macros::vertical;
+use ratatui::style::Stylize;
+use ratatui::widgets::{Block, Borders, Paragraph, Wrap, Widget};
+use ratatui::layout::Constraint::Length;
+use ratatui::Frame;
+use ratatui::crossterm::event;
+use ratatui::layout::{Layout, Rect, Constraint, Direction};
+use ratatui::buffer::Buffer;
+
+
+// Temporary values for world size and stuff while integration isn't working
+const WIDTH: usize = 10;
+const HEIGHT: usize = 10;
+const TILE_SIZE: usize = 2;
+
+pub fn render_loop() -> Result<(), Box<dyn std::error::Error>> {
+    ratatui::run(|terminal| loop {
+        terminal.draw(render)?;
+        if event::read()?.is_key_press() {
+            break Ok(());
+        }
+    })
+}
+
+fn render(frame: &mut Frame) {
+    let world_array: [[u32; WIDTH]; HEIGHT] = [[0; WIDTH]; HEIGHT];
+
+    let grid= frame.area().centered(
+                Length((WIDTH * 2 + 2) as u16),
+                Length((HEIGHT * 2 + 2) as u16)
+            );
+
+    let row_constraints = (0..HEIGHT).map(|_| Constraint::Length(TILE_SIZE as u16));
+    let col_constraints = (0..WIDTH).map(|_| Constraint::Length(TILE_SIZE as u16));
+    let horizontal = Layout::horizontal(col_constraints);
+    let vertical = Layout::vertical(row_constraints);
+
+    let cells = grid.layout_vec(&vertical).into_iter().flat_map(|row| row.layout_vec(&horizontal));
+
+    for (i, cell) in cells.enumerate() {
+        let square = Paragraph::new("╔╗\n╚╝").wrap(Wrap{trim:true});
+        frame.render_widget(square, cell);
+    }
+}
