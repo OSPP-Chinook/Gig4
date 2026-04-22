c0fd0656af87746d6b64624131f7cb16b8ffd942
Didrik <coraxites@gmail.com>
2026-04-22 11:47:03 +0200

    added world border

--- src/player_manager.rs
+++ src/player_manager.rs
@@ -4,12 +4,12 @@ use ratatui::widgets::{Block, Borders, Paragraph, Wrap, Widget};
 use ratatui::layout::Constraint::Length;
 use ratatui::Frame;
 use ratatui::crossterm::event;
-use ratatui::layout::{Layout, Rect, Constraint, Direction};
+use ratatui::layout::{Constraint, Direction, Layout, Margin, Rect};
 use ratatui::buffer::Buffer;
 
 
 // Temporary values for world size and stuff while integration isn't working
-const WIDTH: usize = 10;
+const WIDTH: usize = 20;
 const HEIGHT: usize = 10;
 const TILE_SIZE: usize = 2;
 
@@ -25,10 +25,14 @@ pub fn render_loop() -> Result<(), Box<dyn std::error::Error>> {
 fn render(frame: &mut Frame) {
     let world_array: [[u32; WIDTH]; HEIGHT] = [[0; WIDTH]; HEIGHT];
 
-    let grid= frame.area().centered(
+    let world_area = frame.area().centered(
                 Length((WIDTH * 2 + 2) as u16),
                 Length((HEIGHT * 2 + 2) as u16)
             );
+
+    let grid = world_area.inner(Margin::new(1, 1));
+
+    frame.render_widget(Block::new().borders(Borders::ALL).title("World"), world_area);
 
     let row_constraints = (0..HEIGHT).map(|_| Constraint::Length(TILE_SIZE as u16));
     let col_constraints = (0..WIDTH).map(|_| Constraint::Length(TILE_SIZE as u16));
