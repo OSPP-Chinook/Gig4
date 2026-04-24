a4cb9d82a7076df119e93693961a7fc8cc5d4fab
zayo7445 <zaidoun.younis-khalid.7445@student.uu.se>
2026-04-24 07:10:29 +0200

    Add load with error handling

--- src/assets.rs
+++ src/assets.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+    path::Path,
+    collections::HashMap,
+};
 use serde::Deserialize;
 
 pub type ItemList = Vec<ItemStack>;
@@ -31,7 +34,7 @@ pub struct Building {
     pub y_size: usize,
     pub inventory_size: usize,
     pub production_rules: Vec<String>,
-    pub production_speed: u32,
+    pub production_speed: f32,
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -56,3 +59,55 @@ pub struct Assets {
     pub categories: HashMap<String, Category>,
     pub recipes: HashMap<String, Recipe>,
 }
+
+impl Assets {
+    pub fn load(dir: &Path) -> Result<Self, AssetError> {
+        Ok(Self {
+            items: load_json(&dir.join("items.json"))?,
+            buildings: load_json(&dir.join("buildings.json"))?,
+            categories: load_json(&dir.join("categories.json"))?,
+            recipes: load_json(&dir.join("recipes.json"))?,
+        })
+    }
+}
+
+fn load_json<T>(dir: &Path) -> Result<HashMap<String, T>, AssetError> 
+where T: for<'de> Deserialize<'de> + Identifiable {
+    let asset = std::fs::read_to_string(dir)
+        .map_err(AssetError::IoError)?;
+    
+    let entries: Vec<T> = serde_json::from_str(&asset)
+        .map_err(AssetError::ParseError)?;
+
+    let hashmap = entries.into_iter().map(|e| (e.id().to_owned(), e)).collect();
+    
+    Ok(hashmap)
+}
+
+pub trait Identifiable {
+    fn id(&self) -> &str;
+}
+
+macro_rules! has_id {
+    ($($t:ty), *) => {
+        $(impl Identifiable for $t { fn id(&self) -> &str { &self.id } })*
+    };
+}
+has_id!(Item, Building, Category, Recipe);
+
+#[derive(Debug)]
+pub enum AssetError {
+    IoError(std::io::Error),
+    ParseError(serde_json::Error),
+}
+
+impl std::fmt::Display for AssetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            AssetError::IoError(err) => write!(f, "IO Error: {}", err),
+            AssetError::ParseError(err) => write!(f, "Parse Error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for AssetError {}
