0c328ca2eb00c183533b654d0ef66f5563fd767a
Joel <47787186+joelterenius@users.noreply.github.com>
2026-05-04 01:18:32 +0200

    rewrote render logic. added moveable camera

--- src/player_manager.rs
+++ src/player_manager.rs
@@ -16,12 +16,38 @@ use ratatui::widgets::{Block, Borders, Paragraph};
 // Width and height of a tile on the screen in characters
 const TILE_SIZE: usize = 2;
 
+#[derive(Copy, Clone)]
+struct Camera(i32, i32);
+
+impl Camera {
+    fn change(&mut self, dx: i32, dy: i32) {
+        let width = WIDTH.try_into().unwrap();
+        let height = HEIGHT.try_into().unwrap();
+        let mut x = self.0 + dx;
+        let mut y = self.1 + dy;
+        if x < 0 {x = 0;}
+        if y < 0 {y = 0;}
+        if x >= width {x = width - 1;}
+        if y >= height {y = height - 1;}
+        
+        self.0 = x;
+        self.1 = y;
+    }
+}
+
+// Default: 1. Set to -1 for inverted movement.
+// The default setting looks weird now, but it will make sense when the world is more populated.
+const MOVE_CAMERA: i32 = 1;
+
 pub fn render_loop(
     aid: AID<PlayerManagerMessage>,
     mailbox: Receiver<PlayerManagerMessage>,
     world: AID<WorldManagerMessage>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     ratatui::run(|terminal| {
+        // camera starts centered on the world
+        let mut camera = Camera((WIDTH / 2).try_into().unwrap(), (HEIGHT / 2).try_into().unwrap());
+        
         loop {
             let _ = world.send(WorldManagerMessage::GetDisplay(aid.clone()));
 
@@ -40,7 +66,7 @@ pub fn render_loop(
                 }
             }
 
-            terminal.draw(|frame| render(frame, world_array))?;
+            terminal.draw(|frame| render(frame, world_array, camera))?;
 
             if poll(Duration::from_millis(30))? {
                 match read()? {
@@ -49,6 +75,15 @@ pub fn render_loop(
                             KeyCode::Char('q') => {
                                 break Ok(());
                             }
+                            KeyCode::Char('w') => {camera.change(0, -MOVE_CAMERA);}
+                            KeyCode::Char('s') => {camera.change(0,  MOVE_CAMERA);}
+                            KeyCode::Char('a') => {camera.change(-MOVE_CAMERA, 0);}
+                            KeyCode::Char('d') => {camera.change( MOVE_CAMERA, 0);}
+                            
+                            // KeyCode::Char('w') => {camera = Camera(camera.0, camera.1 - INVERT_CAMERA);}
+                            // KeyCode::Char('s') => {camera = Camera(camera.0, camera.1 + INVERT_CAMERA);}
+                            // KeyCode::Char('a') => {camera = Camera(camera.0 - INVERT_CAMERA, camera.1);}
+                            // KeyCode::Char('d') => {camera = Camera(camera.0 + INVERT_CAMERA, camera.1);}
                             _ => {}
                         }
                     }
@@ -59,49 +94,60 @@ pub fn render_loop(
     })
 }
 
-fn render(frame: &mut Frame, world_array: WorldGrid) {
-    let world_area = frame.area().centered(
-        Length((WIDTH * 2 + 2) as u16),
-        Length((HEIGHT * 2 + 2) as u16),
-    );
-
-    let grid = world_area.inner(Margin::new(1, 1));
-
+fn render(frame: &mut Frame, world_array: WorldGrid, camera: Camera) {
+    let world_area = frame.area().inner(Margin::new(4, 4));
+    
     frame.render_widget(
         Block::new().borders(Borders::ALL).title("World"),
-        world_area,
+        world_area.outer(Margin::new(1, 1)),
     );
-
-    // Vectors av HEIGHT eller WIDTH stycken constraints, alla lika stora som TILE_SIZE
-    let row_constraints = (0..HEIGHT).map(|_| Constraint::Length(TILE_SIZE as u16));
-    let col_constraints = (0..WIDTH).map(|_| Constraint::Length(TILE_SIZE as u16));
-
-    // Split up horizontal and vertical layouts after the rows and columns
-    let horizontal = Layout::horizontal(col_constraints);
-    let vertical = Layout::vertical(row_constraints);
-
-    // Gör en 2d array av cells
-    let rows: Vec<Rect> = grid.layout_vec(&vertical);
-    let grid_array: Vec<Vec<Rect>> = rows
-        .iter()
-        .map(|row: &Rect| row.layout_vec(&horizontal))
-        .collect();
-
-    // TODO: Det här är lätt att förstå men kan vara RUSTigare
-    for (row, y) in grid_array.iter().zip(0..HEIGHT) {
-        for (cell, x) in row.iter().zip(0..WIDTH) {
+    
+    // let origin: (i32, i32) = (0, 0);
+    let box_w = world_area.width / 2;
+    let box_h = world_area.height / 2;
+    
+    for y in 0..box_h {
+        for x in 0..box_w {
+            // draw a background in the "World" area
+            // this is so the player can tell the difference between buildable area and surrounding borders
+            let rect = Rect::new(world_area.x + 2*x, world_area.y + 2*y, 2, 2);
+            let square = Paragraph::new(".").gray();
+            frame.render_widget(square, rect);
+        }
+    }
+    
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
             let tile = &world_array[y][x];
-            // check if tile is empty
+            
+            let y: i32 = y.try_into().unwrap();
+            let x: i32 = x.try_into().unwrap();
+            let draw_pos = (x + (box_w/2) as i32 - camera.0, y + (box_h/2) as i32 - camera.1);
+            let rect_at_pos = if
+                0 <= draw_pos.0 && draw_pos.0 < box_w.into() &&
+                0 <= draw_pos.1 && draw_pos.1 < box_h.into()
+            {
+                // tile in visible grid
+                Rect::new(world_area.x + (2*draw_pos.0 as u16), world_area.y + (2*draw_pos.1 as u16), 2, 2)
+            } else {
+                continue;
+            };
+            
+            
+            
             match tile {
-                Tile::Empty => {}
-                Tile::Worker(aid) => {
-                    let square = Paragraph::new("╭╮\n╰╯").blue();
-                    frame.render_widget(square, *cell);
+                Tile::Empty => {
+                    // overwrite background
+                    let square = Paragraph::new("  \n  ");
+                    frame.render_widget(square, rect_at_pos);
                 }
-                Tile::Building(aid) => {
+                Tile::Worker(_aid) => {
+                    let square = Paragraph::new("╭╮\n╰╯").blue();
+                    frame.render_widget(square, rect_at_pos);
+                }
+                Tile::Building(_aid) => {
                     let square = Paragraph::new("╔╗\n╚╝").red();
-                    frame.render_widget(square, *cell);
-                    // println!("{aid}");
+                    frame.render_widget(square, rect_at_pos);
                 }
             }
         }
