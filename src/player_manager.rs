e225ae89419689ad4bb78e71ad594cada9f490c0
Joel <47787186+joelterenius@users.noreply.github.com>
2026-05-06 06:35:39 +0200

    after merge with main

--- src/player_manager.rs
+++ src/player_manager.rs
@@ -14,7 +14,8 @@ use ratatui::style::Stylize;
 use ratatui::widgets::{Block, Borders, Paragraph};
 
 // Width and height of a tile on the screen in characters
-const TILE_SIZE: usize = 2;
+// Needs to be u16 for ratatui
+const TILE_SIZE: (u16, u16) = (2, 3);
 
 #[derive(Copy, Clone)]
 struct Camera(i32, i32);
@@ -61,6 +62,11 @@ pub fn render_loop(
             (HEIGHT / 2).try_into().unwrap(),
         );
 
+        
+        let mut world_array: WorldGrid =
+                std::array::from_fn(|_| std::array::from_fn(|_| Tile::Empty));
+        let mut old_world_array = world_array.clone();
+        
         loop {
             //read all messages in mailbox
             while let Ok(msg) = mailbox.try_recv() {
@@ -70,7 +76,7 @@ pub fn render_loop(
                 }
             }
 
-            terminal.draw(|frame| render(frame, &world_array, camera))?;
+            terminal.draw(|frame| render(frame, &old_world_array, &world_array, camera))?;
 
             if poll(Duration::from_millis(30))? {
                 match read()? {
@@ -79,18 +85,10 @@ pub fn render_loop(
                             KeyCode::Char('q') => {
                                 break Ok(());
                             }
-                            KeyCode::Char('w') => {
-                                camera.change(0, -MOVE_CAMERA);
-                            }
-                            KeyCode::Char('s') => {
-                                camera.change(0, MOVE_CAMERA);
-                            }
-                            KeyCode::Char('a') => {
-                                camera.change(-MOVE_CAMERA, 0);
-                            }
-                            KeyCode::Char('d') => {
-                                camera.change(MOVE_CAMERA, 0);
-                            }
+                            KeyCode::Char('w') => {camera.change(0, -MOVE_CAMERA);}
+                            KeyCode::Char('s') => {camera.change(0,  MOVE_CAMERA);}
+                            KeyCode::Char('a') => {camera.change(-MOVE_CAMERA, 0);}
+                            KeyCode::Char('d') => {camera.change( MOVE_CAMERA, 0);}
                             _ => {}
                         }
                     }
@@ -101,24 +99,48 @@ pub fn render_loop(
     })
 }
 
-fn render(frame: &mut Frame, world_array: &WorldGrid, camera: Camera) {
-    let world_area = frame.area().inner(Margin::new(4, 4));
+fn is_same_tile(old_tile: &Tile, new_tile: &Tile) -> bool {
+    match old_tile {
+        Tile::Empty => {
+            false
+        }
+        Tile::Worker(aid) => {
+            match new_tile {
+                Tile::Worker(aid_new) => {
+                    return aid == aid_new;
+                }
+                _ => false
+            }
+        }
+        Tile::Building(aid) => {
+            match new_tile {
+                Tile::Building(aid_new) => {
+                    return aid == aid_new;
+                }
+                _ => false
+            }
+        }
+    }
+}
 
+fn render(frame: &mut Frame, old_world_array: &WorldGrid, world_array: &WorldGrid, camera: Camera) {
+    let world_area = frame.area().inner(Margin::new(4, 4));
+    
     frame.render_widget(
         Block::new().borders(Borders::ALL).title("World"),
         world_area.outer(Margin::new(1, 1)),
     );
 
-    let box_w = world_area.width / 2;
-    let box_h = world_area.height / 2;
+    let box_w = world_area.width / TILE_SIZE.0;
+    let box_h = world_area.height / TILE_SIZE.1;
 
     for y in 0..box_h {
         for x in 0..box_w {
             // draw a background in the "World" area
             // this is so the player can tell the difference between buildable area and surrounding borders
-            let rect = Rect::new(world_area.x + 2 * x, world_area.y + 2 * y, 2, 2);
+            let rect = Rect::new(world_area.x + TILE_SIZE.0 * x, world_area.y + TILE_SIZE.1 * y, TILE_SIZE.0, TILE_SIZE.1);
             let square = Paragraph::new(".").gray();
-            frame.render_widget(square, rect);
+            // frame.render_widget(square, rect);
         }
     }
 
@@ -128,41 +150,46 @@ fn render(frame: &mut Frame, world_array: &WorldGrid, camera: Camera) {
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
             let tile = &world_array[y][x];
-
+            let old_tile_n = &old_world_array[y-1][x];
+            let old_tile_s = &old_world_array[y+1][x];
+            let old_tile_w = &old_world_array[y][x-1];
+            let old_tile_e = &old_world_array[y][x+1];
+            
             let y: i32 = y.try_into().unwrap();
             let x: i32 = x.try_into().unwrap();
             let draw_pos = (
-                x + (box_w / 2) as i32 - camera.0,
-                y + (box_h / 2) as i32 - camera.1,
+                x + (box_w / TILE_SIZE.0) as i32 - camera.0,
+                y + (box_h / TILE_SIZE.1) as i32 - camera.1,
             );
-            let rect_at_pos = if 0 <= draw_pos.0
-                && draw_pos.0 < box_w.into()
-                && 0 <= draw_pos.1
-                && draw_pos.1 < box_h.into()
+            let mut rect_at_pos = if
+                0 <= draw_pos.0 && draw_pos.0 < box_w.into() &&
+                0 <= draw_pos.1 && draw_pos.1 < box_h.into()
             {
                 // tile in visible grid
-                Rect::new(
-                    world_area.x + (2 * draw_pos.0 as u16),
-                    world_area.y + (2 * draw_pos.1 as u16),
-                    2,
-                    2,
-                )
+                let rx = world_area.x + TILE_SIZE.0*(draw_pos.0 as u16);
+                let ry = world_area.y + TILE_SIZE.1*(draw_pos.1 as u16);
+                Rect::new(rx, ry, TILE_SIZE.0, TILE_SIZE.1)
             } else {
                 continue;
             };
-
+            
+            if is_same_tile(old_tile_n, tile) {rect_at_pos.y -= 1}
+            if is_same_tile(old_tile_s, tile) {rect_at_pos.y += 1}
+            if is_same_tile(old_tile_w, tile) {rect_at_pos.x -= 1}
+            if is_same_tile(old_tile_e, tile) {rect_at_pos.x += 1}
+            
             match tile {
                 Tile::Empty => {
                     // overwrite background
-                    let square = Paragraph::new("  \n  ");
-                    frame.render_widget(square, rect_at_pos);
+                    // let square = Paragraph::new("  \n  ");
+                    // frame.render_widget(square, rect_at_pos);
                 }
                 Tile::Worker(_aid) => {
-                    let square = Paragraph::new("╭╮\n╰╯").blue();
+                    let square = Paragraph::new("╭—╮\n╰—╯").blue();
                     frame.render_widget(square, rect_at_pos);
                 }
                 Tile::Building(_aid) => {
-                    let square = Paragraph::new("╔╗\n╚╝").red();
+                    let square = Paragraph::new("╔═╗\n╚═╝").red();
                     frame.render_widget(square, rect_at_pos);
                 }
             }
